4d44cd09680d68f39e08a671b8cad99ea9ce9974
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-17 20:19:28 -0300

    Add playerSelection function
     This function returns the user choice ('rock', 'paper' or 'scissors' (case insensitive)). If the answer is not valid it keeps asking until it receives a valid input.

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function getComputerChoice() {
 
     return computerChoice;
 }
+
+
+// asks user's choice
+function playerSelection() {
+    let checkChoice = 1; 
+    let playerChoice;
+    // checks if the user's input is valid. Continues the loop until it receives a valid answer.
+    while (checkChoice) {
+        playerChoice = prompt("Shoot! (Choose 'Rock', 'Paper' or 'Scissors')").toLowerCase(); // case insensitive
+        if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
+            checkChoice = 0; // valid answer -> breaks the loop
+        } else{
+            alert("Invalid answer. Please choose 'Rock', 'Paper' or 'Scissors'");
+        }
+    }
+    return playerChoice
+}
+
