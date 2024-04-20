c6917f071c8621ce0298ca95439abf6684fa7a7b
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-20 17:18:28 -0300

    Remove old commented code

--- script.js
+++ script.js
@@ -15,24 +15,6 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-/*
-// asks user's choice
-function getPlayerChoice() {
-    let checkChoice = 1; 
-    let playerChoice;
-    // checks if the user's input is valid. Continues the loop until it receives a valid answer.
-    while (checkChoice) {
-        playerChoice = prompt("Shoot! (Choose 'Rock', 'Paper' or 'Scissors')").toLowerCase(); // case insensitive
-        if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-            checkChoice = 0; // valid answer -> breaks the loop
-        } else{
-            alert("Invalid answer. Please choose 'Rock', 'Paper' or 'Scissors'");
-        }
-    }
-    return playerChoice
-}
-
-*/
 let computerScore = 0; 
 let playerScore = 0;
 
@@ -96,14 +78,3 @@ document.querySelector("#paper").addEventListener("click", function() {
 document.querySelector("#scissors").addEventListener("click", function() {
     playSingleRound("scissors");
 });
-
-/*
-function playGame() {
-    // five round game
-    for(let i = 0; i < 5; i++) {
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        alert(playSingleRound(computerSelection, playerSelection));
-    }
-}
-*/
